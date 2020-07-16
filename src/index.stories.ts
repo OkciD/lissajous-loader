77c42acbe4b466d57bf51a77ec8c65c68a7420c1
Yury Baykov <y.baykov@corp.mail.ru>
2020-07-17 00:46:12 +0300

    knobs

--- src/index.stories.ts
+++ src/index.stories.ts
@@ -1,11 +1,16 @@
+import { withKnobs, number } from "@storybook/addon-knobs";
 import LissajousLoader from './index';
 
-export default { title: 'LissajousLoader' };
+export default {
+	title: 'LissajousLoader',
+	decorators: [withKnobs]
+};
 
 export const main = () => {
 	const canvas = document.createElement('canvas');
-	canvas.width = 120;
-	canvas.height = 120;
+	const canvasSize = number('Canvas size', 80);
+	canvas.width = canvasSize;
+	canvas.height = canvasSize;
 
 	const loader = new LissajousLoader(canvas);
 	loader.start();
