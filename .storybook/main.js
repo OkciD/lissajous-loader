9e89619e175a4b814a4cf4da87cc7cac96cfc539
Yury Baykov <y.baykov@corp.mail.ru>
2020-07-21 00:15:33 +0300

    no tsx

--- .storybook/main.js
+++ .storybook/main.js
@@ -3,14 +3,14 @@ module.exports = {
 	addons: ['@storybook/addon-knobs/register'],
 	webpackFinal: async (config) => {
 		config.module.rules.push({
-			test: /\.(ts|tsx)$/,
+			test: /\.ts$/,
 			use: [
 				{
 					loader: require.resolve('ts-loader'),
 				},
 			],
 		});
-		config.resolve.extensions.push('.ts', '.tsx');
+		config.resolve.extensions.push('.ts');
 		return config;
 	},
 };
