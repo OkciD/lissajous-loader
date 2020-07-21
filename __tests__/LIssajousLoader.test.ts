80074306209b923f613226138aca9ac11245aec4
Yury Baykov <y.baykov@corp.mail.ru>
2020-07-22 00:58:06 +0300

    one more raf test

--- __tests__/LIssajousLoader.test.ts
+++ __tests__/LIssajousLoader.test.ts
@@ -1,30 +1,46 @@
 let requestAnimationFrameMock: jest.SpyInstance;
 let cancelAnimationFrameMock: jest.SpyInstance;
 
+jest.useFakeTimers();
+
 describe('LissajousLoader', () => {
 	beforeEach(() => {
 		requestAnimationFrameMock = jest.spyOn(window, 'requestAnimationFrame')
 			.mockImplementation((callback: FrameRequestCallback) => {
-				return setTimeout(callback, 0);
+				return setTimeout(callback, 16);
 			});
 
 		cancelAnimationFrameMock = jest.spyOn(window, 'cancelAnimationFrame')
 			.mockImplementation((requestId: number) => {
 				return clearTimeout(requestId);
 			});
-
-		jest.useFakeTimers()
 	});
 
-	it('test test', () => {
+	it('raf test 1', () => {
 		let lol = 4
 
 		window.requestAnimationFrame(() => {
 			lol += 5;
 		});
-		jest.runAllTimers();
 
-		expect(lol).toStrictEqual(9);
+		jest.advanceTimersByTime(5);
+		expect(lol).toStrictEqual(4);
+
+		jest.advanceTimersByTime(20);
+		expect(lol).toStrictEqual(5);
+	});
+
+	it('raf test 2', () => {
+		let lol = 4
+
+		const requestId = window.requestAnimationFrame(() => {
+			lol += 5;
+		});
+		jest.advanceTimersByTime(5);
+		window.cancelAnimationFrame(requestId);
+
+		jest.runAllTimers();
+		expect(lol).toStrictEqual(4);
 	});
 
 	afterEach(() => {
