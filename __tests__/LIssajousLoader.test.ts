85ef484e7ee05a16fc1f66d61c399bbb3d0d1d52
Yury Baykov <y.baykov@corp.mail.ru>
2020-07-25 21:22:17 +0300

    + tests

--- __tests__/LIssajousLoader.test.ts
+++ __tests__/LIssajousLoader.test.ts
@@ -12,6 +12,7 @@ let canvas: HTMLCanvasElement;
 const RAF_TIMEOUT = 16;
 const POINTS_COUNT = 125;
 const ITERATIONS_COUNT = POINTS_COUNT + 1;
+const FULL_CYCLE_ITERATIONS_COUNT = 2 * ITERATIONS_COUNT + 1;
 const PAUSE = 1000;
 const defaultProps: Props = {
 	xFrequency: 3,
@@ -43,21 +44,43 @@ describe('LissajousLoader', () => {
 	it('should render nothing without start() called', () => {
 		const loader = new LissajousLoader(canvas, defaultProps);
 
+		jest.runAllTimers();
 		expect(canvas).toMatchSnapshot();
 	});
 
 	it(`should render the whole figure after ${ITERATIONS_COUNT} iterations`, () => {
 		const loader = new LissajousLoader(canvas, defaultProps);
 		loader.start();
-		jest.advanceTimersByTime(ITERATIONS_COUNT * RAF_TIMEOUT);
 
+		jest.advanceTimersByTime(ITERATIONS_COUNT * RAF_TIMEOUT);
 		expect(canvas).toMatchSnapshot();
 	});
 
-	it.skip(`should clear a figure after 2 * ${ITERATIONS_COUNT} iterations (+ pause)`, () => {
+	it(`should clear a figure after ${FULL_CYCLE_ITERATIONS_COUNT} iterations (+ pause)`, () => {
 		const loader = new LissajousLoader(canvas, defaultProps);
 		loader.start();
-		jest.advanceTimersByTime(2 * (ITERATIONS_COUNT - 1) * RAF_TIMEOUT + PAUSE);
+
+		jest.advanceTimersByTime(FULL_CYCLE_ITERATIONS_COUNT * RAF_TIMEOUT + PAUSE);
+		expect(canvas).toMatchSnapshot();
+	});
+
+	it.skip(`should wait for ${PAUSE}ms when rendered the whole figure`, () => {
+		const loader = new LissajousLoader(canvas, defaultProps);
+		loader.start();
+
+		jest.advanceTimersByTime(ITERATIONS_COUNT * RAF_TIMEOUT);
+		// expect(canvas).toMatchSnapshot();
+
+		requestAnimationFrameMock.mockReset();
+		expect(requestAnimationFrameMock).not.toBeCalled();
+
+		jest.advanceTimersByTime(PAUSE);
+		expect(requestAnimationFrameMock).not.toBeCalled();
+
+		jest.advanceTimersByTime(RAF_TIMEOUT);
+		expect(requestAnimationFrameMock).toBeCalledTimes(1);
+
+		jest.advanceTimersByTime(10 * RAF_TIMEOUT);
 
 		expect(canvas).toMatchSnapshot();
 	});
